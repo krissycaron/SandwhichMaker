b63f68261337253d31484565e78cb647fde533ad
Kristin Caron <krissycaron@gmail.com>
2017-02-22 20:27:30 -0600

    started my iffe

--- Meat.js
+++ Meat.js
@@ -2,13 +2,27 @@
 var SandwichMaker = (function(maker) {
 
   // Private variable to store the different meat prices
-  var meatPrices;
+  var meatPrices = {
+  	ham: 2.00,
+  	turkey: 3.00, 
+  	roastbeef: 4.00, 
+  	salami: 1.00
+  }
+// console.log(meatPrices.turkey); // prints to console the price of the meat
+ })(SandwichMaker)
+
+
+
+
+
+
 
   // Augment the original object with another method
-  maker.addMeat = function() {
-    return ???;
-  };
+  // maker.addMeat = function() {
+
+  //   return ???;
+  // };
 
   // Return the new, augmented object with the new method on it
-  return maker;
-})(SandwichMaker);
+//   return maker;
+// })(SandwichMaker);
